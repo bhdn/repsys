4a5bef28c9b44dee567e92b281b49e437d1e081e
Bogdano Arendartchuk <bogdano@mandriva.com.br>
2007-04-23 17:27:24 -0300

    Fixed keyword name used to the function commit() to "target", as was expected by the command line parser.

--- RepSys/rpmutil.py
+++ RepSys/rpmutil.py
@@ -363,24 +363,24 @@ def checkout(pkgdirurl, path=None, revision=None):
         print "checking out from mirror", current
     svn.checkout(current, path, rev=revision, show=1)
 
-def commit(path=".", message=None):
+def commit(target=".", message=None):
     svn = SVN(noauth=True)
-    info = svn.info2(path)
+    info = svn.info2(target)
     url = info.get("URL")
     if url is None:
         raise Error, "working copy URL not provided by svn info"
     if mirror.enabled():
-        newurl = mirror.switchto_parent(svn, url, path)
+        newurl = mirror.switchto_parent(svn, url, target)
         print "relocated to", newurl
     try:
         # we can't use the svn object here because pexpect hides VISUAL
         mopt = ""
         if message is not None:
             mopt = "-m \"%s\"" % message
-        os.system("svn ci %s %s" % (mopt, path))
+        os.system("svn ci %s %s" % (mopt, target))
     finally:
         if mirror.enabled():
-            mirror.switchto_mirror(svn, newurl, path)
+            mirror.switchto_mirror(svn, newurl, target)
             print "relocated back to", url
 
 def get_submit_info(path):
