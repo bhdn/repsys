c96695a021bc732f4e2c788ecc92c30300946486
Bogdano Arendartchuk <bogdano@mandriva.com.br>
2007-05-02 12:10:09 -0300

    Added help system for plugins.
    The help messages are extracted from the docstring of the plugin module.
    The user can access them through --help-plugins and --help-plugin <name>.

--- RepSys/plugins/__init__.py
+++ RepSys/plugins/__init__.py
@@ -1,12 +1,27 @@
 import os
 
+loaded = {}
+
 def load():
     # based on smart's plugin system 
     pluginsdir = os.path.dirname(__file__)
     for entry in os.listdir(pluginsdir):
         if entry != "__init__.py" and entry.endswith(".py"):
-            __import__("RepSys.plugins."+entry[:-3])
+            name = entry[:-3]
+            loaded[name] = __import__("RepSys.plugins."+name,
+                    fromlist=[name])
         elif os.path.isdir(entry):
             initfile = os.path.join(entry, "__init__.py")
             if os.path.isfile(initfile):
-                __import__("RepSys.plugins."+entry)
+                loaded[entry] = __import__("RepSys.plugins."+entry,
+                        fromlist=[entry])
+
+def list():
+    return loaded.keys()
+
+def help(name):
+    from RepSys import Error
+    try:
+        return loaded[name].__doc__
+    except KeyError:
+        raise Error, "plugin %s not found" % name
