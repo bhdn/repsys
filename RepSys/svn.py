926ec3285dc0fd2ea73af3517d483e291edb1f00
Bogdano Arendartchuk <bogdano@mandriva.com.br>
2007-11-15 03:14:04 -0200

    Handle those authorization errors related to HTTP

--- RepSys/svn.py
+++ RepSys/svn.py
@@ -33,6 +33,10 @@ class SVN:
                         "Seems ssh-agent or ForwardAgent are not setup, see "
                         "http://wiki.mandriva.com/en/Development/Docs/Contributor_Tricks#SSH_configuration"
                         " for more information." % e)
+            elif "authorization failed":
+                raise Error, ("%s\n"
+                        "Note that repsys does not support any HTTP "
+                        "authenticated access." % e)
             raise
 
     def _execsvn_success(self, *args, **kwargs):
